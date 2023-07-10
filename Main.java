f694d2ef9ebb36b92feeac15f2941add6765f3ac
DaniCoimbra <dcsalomao2000@gmail.com>
2023-07-10 17:36:17 -0600

    Sorting algorithms

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import sorting.InsertionSort;
+import sorting.SelectionSort;
+
 public class Main {
     public static void main(String[] args){
-
+        int[] array = {3,2,4,1,5};
+        InsertionSort.insertionSort(array);
+        for(int i = 0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
     }
 }
